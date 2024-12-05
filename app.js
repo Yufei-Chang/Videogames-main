25ed5ed9fa6a86cb2376d9c9723625acf25b7b88
Yufei-Chang <yufei.chang@hotmail.com>
2024-12-05 12:59:03 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const gamesRouters = require("./routers/videogames");
+
+app.use(express.json());
+app.use("/videogames", gamesRouters);
+
+
+app.get('/', (req, res) => {
+    res.json('Hello world');
+});
+
+app.listen(port, () => {
+    console.log('server è partito');
+    
+});
---
+++ routers/videogames.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const videogameController = require("../controllers/videogameController")
+
+// index
+router.get('/', videogameController.index);
+
+// show
+router.get('/:id', videogameController.show);
+
+// create
+router.post('/', videogameController.create);
+
+// update
+router.put('/:id', videogameController.update);
+
+// modify
+router.patch('/:id', videogameController.modify);
+
+// destroy
+router.delete('/:id', videogameController.destroy);
+
+
+
+module.exports = router;
